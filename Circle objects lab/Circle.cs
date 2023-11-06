a703961dfa9b7f1d5822a1dd5958556e366540c2
Benjamin Holmes <manbej.sholem@gmail.com>
2023-11-06 10:18:34 -0500

    Add project files.

---
+++ Circle objects lab/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_objects_lab
+{
+    public class Circle
+    {
+        
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        private double Radius { get; set; }
+        public double Diameter => 2 * Radius;
+
+        public double Circumference => 2 * Radius * System.Math.PI;
+
+        public double Area => System.Math.PI * (Radius * Radius);
+
+        public void Grow()
+        {
+            Radius = Radius * 2;
+        }
+
+        public double GetRadius() 
+        {
+            return Radius; 
+        }
+
+    }
+}
---
+++ Circle objects lab/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Circle_objects_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ShowCirlceMeasurments();
+            
+        }
+
+
+
+        static double Validator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the radius of your circle");
+                string userRadius = Console.ReadLine();
+                if (double.TryParse(userRadius, out double radius))
+                {
+                    return radius;
+                }
+
+                Console.WriteLine("Invalid input, press and key to try again");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
+        }
+
+        static void ShowCirlceMeasurments()
+        {
+            double radius = Validator();
+            Circle userCircle = new Circle(radius);
+
+            Console.WriteLine($"Radius: {userCircle.GetRadius()}");
+            Console.WriteLine($"Diameter: {userCircle.Diameter}");
+            Console.WriteLine($"Area: {userCircle.Area}");
+            while (true)
+            {
+                Console.WriteLine("Do you want to grow your circle? yes/no");
+                string anwser = Console.ReadLine().ToUpper().Trim();
+                if (anwser.Equals("YES"))
+                {
+                    userCircle.Grow();
+                    Console.WriteLine($"Radius: {userCircle.GetRadius()}");
+                    Console.WriteLine($"Diameter: {userCircle.Diameter}");
+                    Console.WriteLine($"Area: {userCircle.Area}");
+                }
+                else
+                {
+                    Console.WriteLine("Later nerd");
+                    break;
+                }
+            }
+        }
+
+    }
+}
